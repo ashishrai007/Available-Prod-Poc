cf7d6968520776aad1ba61eeed7e3a3786ca0b7e
ashishrai007 <ashishrai007@gmail.com>
2022-05-25 20:47:44 +0530

    Initial Comit

---
+++ force-app/main/default/lwc/availableProducts/availableProducts.js
@@ -0,0 +1,91 @@
+import { LightningElement, wire, api,track } from 'lwc';
+import getAvailableProducts from '@salesforce/apex/OrderController.getAvailableProducts';
+import addtoOrder from '@salesforce/apex/OrderController.addtoOrder';
+import getOrderDetails from '@salesforce/apex/OrderController.getOrderDetails';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+
+const COLS = [
+    { label: 'Name', fieldName: 'Name',editable: false,displayReadOnlyIcon: true,sortable:true },
+    { label: 'List Price', fieldName: 'UnitPrice',editable: false,displayReadOnlyIcon: true}
+];
+
+export default class AvailableProducts extends LightningElement {
+
+    
+    @api recordId;  
+    @track products;
+    @track orderDetails;
+    error;    
+    columns = COLS;
+    buttonvisible=false;
+    buttonDisable = false;
+    
+
+    _wiredData;
+    @wire(getAvailableProducts, { orderId: '$recordId' })
+    availableProducts(wireResult){
+        const { data, error } = wireResult;
+        this._wiredData = wireResult;
+        if(data){
+            console.log("OppData", data);
+            this.products = data;
+        }
+        if(error) {
+            this.error = error;
+        }
+    }
+
+    _wiredData2;
+    @wire(getOrderDetails, { orderId: '$recordId' })
+    orderDetails(wireResult){
+        const { data, error } = wireResult;
+        this._wiredData2 = wireResult;
+        if(data){
+            console.log("OppData", data);
+            this.orderDetails = data;
+            if(this.orderDetails && this.orderDetails.Status == 'Activated') {
+                this.buttonDisable = true;
+            }
+        }
+        if(error) {
+            this.error = error;
+        }
+    }
+       
+        //This method is used to stored the product to order product component
+        addtoOrder() {
+        
+        var selectedRecords =  this.template.querySelector("lightning-datatable").getSelectedRows();
+            
+        // Calling the apex method with parameters orderid and products to save the record
+        addtoOrder({ products : selectedRecords, orderId : this.recordId }) 
+            .then(result => {
+                this.message = result;
+                this.error = undefined;
+                if(this.message !== undefined) {
+                    this.dispatchEvent(
+                        new ShowToastEvent({
+                            title: 'Success',
+                            message: 'Product(s) added to order successfully!',
+                            variant: 'success',
+                        }),
+                    );
+                }
+                             
+                this.template.querySelector('lightning-datatable').selectedRows=[];
+                this.template.querySelector('c-order-products').handleClick();
+            })
+            .catch(error => {
+                this.message = undefined;
+                this.error = error;
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'There was an error in adding Products',
+                        variant: 'error',
+                    }),
+                );                
+            });       
+        return;  
+    }
+}
---
+++ force-app/main/default/lwc/orderProducts/orderProducts.js
@@ -0,0 +1,81 @@
+import { LightningElement, wire, api,track } from 'lwc';
+import getOrderProducts from '@salesforce/apex/OrderController.getOrderProducts';
+import activateOrder from '@salesforce/apex/OrderController.activateOrder';
+import { refreshApex } from '@salesforce/apex';
+import { updateRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+
+const COLS = [
+    { label: 'Name', fieldName: 'productName',editable: false,displayReadOnlyIcon: true,sortable:true },
+    { label: 'UnitPrice', fieldName: 'UnitPrice',editable: false,displayReadOnlyIcon: true},
+    { label: 'Quantity', fieldName: 'Quantity',editable: false,displayReadOnlyIcon: true },
+    { label: 'TotalPrice', fieldName: 'TotalPrice',editable: false,displayReadOnlyIcon: true}
+];
+
+export default class OrderProducts extends LightningElement {
+
+    @api recordId;
+    @api responseTest;
+    error;    
+    columns = COLS;
+    @track buttonDisable = false;
+
+    _wiredData;
+        @wire(getOrderProducts, { orderId: '$recordId' })
+        orderProducts(wireResult){
+            const { data, error } = wireResult;
+            this._wiredData = wireResult;
+            if(data){
+                this.responseTest = data.map(row=>{
+                return{...row, productName: row.Product2.Name, OrderStatus:row.Order.Status}
+                });
+                
+                if(this.responseTest && this.responseTest[0].OrderStatus == 'Activated') {
+                      this.buttonDisable = true;
+                  }
+
+            }
+            if(error) {
+                console.error(error);
+            }
+        }
+        connectedCallback() {
+            console.log("recordId==>", this.recordId);
+        }
+        // To refresh orderProduct data table
+        @api handleClick() {
+             refreshApex(this._wiredData);
+        }
+
+
+    activateOrder() { 
+        
+        activateOrder({ ordProducts : this.responseTest }) // from here passing orderitems ids to apex and getting response result
+            .then(result => {
+                this.message = result;
+                this.error = undefined;
+                if(this.message !== undefined) {
+                    this.dispatchEvent(
+                        new ShowToastEvent({
+                            title: 'Success',
+                            message: 'Order Activated successfully!',
+                            variant: 'success',
+                        }),
+                    );
+                }
+                    return refreshApex(this._wiredData);
+            })
+            .catch(error => {
+                this.message = undefined;
+                this.error = error;
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'There was an error activating Order',
+                        variant: 'error',
+                    }),
+                );
+            });
+    }
+    
+}
